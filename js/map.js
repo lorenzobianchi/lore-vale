2506483d5f5f407998eff971d2e3b54bfe6abdd5
lorenzobianchi <lorenzo.bianchi@inmagik.com>
2017-03-13 16:15:11 +0100

    styling map

--- js/map.js
+++ js/map.js
@@ -1,6 +1,7 @@
 function initMap() {
   var uluru = {lat: 45.630302, lng: 9.762596};
   var map = new google.maps.Map(document.getElementById('map'), {
+    gestureHandling: "none",
     zoom: 12,
     center: uluru
   });
