d4e66a5182484058f5da10f72ea0c5f8615b9caa
lorenzobianchi <lorenzo.bianchi@inmagik.com>
2017-06-06 16:41:50 +0200

    scrollwheel:false in map.js

--- js/map.js
+++ js/map.js
@@ -3,7 +3,8 @@ function initMap() {
   var map = new google.maps.Map(document.getElementById('map'), {
     gestureHandling: "none",
     zoom: 12,
-    center: uluru
+    center: uluru,
+    scrollwheel: false,
   });
   var marker = new google.maps.Marker({
     position: uluru,
